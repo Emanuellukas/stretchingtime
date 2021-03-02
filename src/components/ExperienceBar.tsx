e358a613c0fb86828ec2df535d9063f7a8775832
Emanuellukas <lucasesn23@gmail.com>
2021-03-02 20:17:04 -0300

    Primeira implementação do Styles Modules do Next, dentro da ExperienceBar.tsx

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,15 @@
+import styles from '../styles/components/ExperienceBar.module.scss'
 import {useState} from 'react'
 
 export function ExperienceBar() {
     const [hovered, setHovered] = useState(false);
     return (
-        <header className='experience-bar' onMouseOver={() => setHovered(true)} onMouseOut={() => setHovered(false)}>
+        <header className={styles.experienceBar} onMouseOver={() => setHovered(true)} onMouseOut={() => setHovered(false)}>
             <span>0 xp</span>
             <div>
                 <div style={{width: '50%'}}/>
                 {hovered && (
-                    <span className={`current-experience ${hovered ? 'hovered' : ''}`} style={{left: '50%'}}>300xp</span>
+                    <span className={`${styles.currentExperience} ${hovered ? 'hovered' : ''}`} style={{left: '50%'}}>300xp</span>
                 )}
             </div>
             <span>600 xp</span>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ExperienceBar from "../components/ExperienceBar";
 
+import ExperienceBar from "../components/ExperienceBar";
 export default function Home() {
     return (
         <div className='container'>
